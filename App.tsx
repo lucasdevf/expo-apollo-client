5ee08074da40be2e2e2f87b69846903bb6d329d7
Lucas Ferreira <lucaasferreirafl@gmail.com>
2023-09-12 14:29:00 -0300

    feat: implements @apollo/client

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
+import { ApolloClient, ApolloProvider, gql, InMemoryCache } from '@apollo/client';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+})
+
 export default function App() {
+  client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
 
